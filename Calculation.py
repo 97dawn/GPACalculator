6f8cef8df11ff64dd6f71c5ca57815aa025b648e
Doeun Kim <graceee318@gmail.com>
2018-01-16 23:26:32 +0900

    seventh commit : complete version 1.0 application with three SUNY schools

--- Calculation.py
+++ Calculation.py
@@ -10,35 +10,64 @@ from Binghamton import Binghamton
 from StonyBrook import StonyBrook
 
 class Calculation(Frame):
+    
     def clear(self,root):
         list = root.grid_slaves()
         for l in list:
             l.destroy()
+            
     def __init__(self,root,uni,num):
         self.clear(root)
+        self.courses=num
         gpaLabel = Label(root,font=('arial',10,'bold'), text="GPA",bd=10,anchor='w')
         gpaLabel.grid(row=0, column=0)
-        gpa = Entry(root,width=10);
-        gpa.grid(row=0, column=1)
+        self.gpa = Entry(root,width=10);
+        self.gpa.grid(row=0, column=1)
         
         outof = Label(root,font=('arial',10,'bold'), text=" / 4.0",bd=10,anchor='w')
         outof.grid(row=0, column=2)
         
+        self.courseCredits = []
+        self.courseGrades = []
         for i in range(1,int(num)+1):
             courseLabel = Label(root,font=('arial',10,'bold'), text="Course "+str(i),bd=10,anchor='w')
             courseLabel.grid(row=i, column = 0)
             courseCredit = Entry(root,width=10)
             courseCredit.grid(row=i,column=1)
+            self.courseCredits.append(courseCredit)
             creditLabel = Label(root,font=('arial',10,'bold'), text="credits",bd=10,anchor='w')
             creditLabel.grid(row=i, column=2)
             letterGrade = Combobox(root,width=10)
             if uni == 'University at Albany':
-                letterGrade['values'] = Albany().getLetterGrades()
+                letterGrade['values'] = Albany().getLetterGrades()['LetterGrades']
+                self.letterGrades = Albany().getLetterGrades()['LetterGrades']
+                self.grades =  Albany().getLetterGrades()['Grades']
             elif uni == 'Binghamton University':
-                letterGrade['values'] = Binghamton().getLetterGrades()
+                letterGrade['values'] = Binghamton().getLetterGrades()['LetterGrades']
+                self.letterGrades = Binghamton().getLetterGrades()['LetterGrades']
+                self.grades =  Binghamton().getLetterGrades()['Grades']
             elif uni == 'Stony Brook University':
-                letterGrade['values'] = StonyBrook().getLetterGrades()
-            letterGrade.grid(row=i, column=3)
+                letterGrade['values'] = StonyBrook().getLetterGrades()['LetterGrades']
+                self.letterGrades = StonyBrook().getLetterGrades()['LetterGrades']
+                self.grades =  StonyBrook().getLetterGrades()['Grades']
             
-        calculate = Button(root,text="Calculate")
+            letterGrade.grid(row=i, column=3)
+            self.courseGrades.append(letterGrade)
+            
+        calculate = Button(root,text="Calculate", command=self.calculate)
         calculate.grid(row=int(num)+1,column=1,padx=10, pady=10)
+    
+    def calculate(self):
+        self.gpa.delete(0, END)
+        gpa=0
+        credits=0
+        for i in range(len(self.courseCredits)):
+            grade = self.courseGrades[i].get() # letter grade
+            credit = int(self.courseCredits[i].get())
+            credits += credit
+            grade = self.grades[self.letterGrades.index(grade)]
+            gpa += credit * grade            
+        gpa /= credits
+        self.gpa.insert(0, round(gpa,2))
+            
+            
--- UniData/Binghamton.py
+++ UniData/Binghamton.py
@@ -12,6 +12,6 @@ class Binghamton(object):
 
     def getLetterGrades(self):
         letterGrades = ['A','A-','B+','B','B-','C+','C','C-','D','F']
-        grades=[4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.0,0.0]
+        grades=[4.00,3.70,3.30,3.00,2.70,2.30,2.00,1.70,1.00,0.00]
         return {'LetterGrades':letterGrades,'Grades':grades}
         
